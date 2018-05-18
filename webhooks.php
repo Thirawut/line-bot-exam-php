2af7fc87e7c5f7ed0bf846c0aa7441c81074cbcd
Thirawut <38032132+Thirawut@users.noreply.github.com>
2018-05-18 11:48:14 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			//$text = $event['source']['userId'];
-			$text ='sdsdfsf'
+			$text ="sdsdfsf";
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
