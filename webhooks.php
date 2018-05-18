5cdd23688975b5eb724efbb8bfc5abfd35fbbfe3
Thirawut <38032132+Thirawut@users.noreply.github.com>
2018-05-18 13:42:33 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -18,13 +18,19 @@ if (!is_null($events['events'])) {
 			// Get text sent
 			//$text = $event['source']['userId'];
 			$text ="sdsdfsf";
+			$text1 ="2125";
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
 			$messages = [
-			'type' => 'text',
-			'text' => $text 
+				{'type' => 'text',
+			'text' => $text },
+				{'type' => 'text',
+			'text' => $text1 }
+			
 			];
+			
+			
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
