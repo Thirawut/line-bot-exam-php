127114dc5b8b7bfee542e6f4b55b48dda1484970
Thirawut <38032132+Thirawut@users.noreply.github.com>
2018-05-18 11:45:26 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,15 +16,13 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			//$text = $event['source']['userId'];
-			$text=$event['message'];
+			$text = $event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-			//$replyToken ='555'
 			// Build message to reply back
 			$messages = [
 			'type' => 'text',
-			'text' => $text + "888"
+			'text' => $text 
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
