8113ac7334c8d4ce91aeed418be23561701e4e6a
Thirawut <38032132+Thirawut@users.noreply.github.com>
2018-05-18 15:09:26 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -25,19 +25,26 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
 			
-			if ($event['message']['text'] == "22")
+			if ($event['message']['text'] == Hi")
 			{
 			$messages = [
 				'type' => 'text',
-			        'text' => $text1
+			        'text' => "Hi"
 			];
 			}
-			else if ($event['message']['text'] == "1")
+			else if ($event['message']['text'] == "Hello")
 			{
 			$messages = [
 				'type' => 'text',
-			        'text' => "123"
+			        'text' => "สวัสดี"
 			];
+			}
+			else if ($event['message']['text'] == "pic")
+			{
+
+			$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
+			$outputText = new LINE\LINEBot\MessageBuilder\ImageMessageBuilder($img_url, $img_url);
+			$response = $bot->replyMessage($event->getReplyToken(), $outputText);
 			}
 			
 			
